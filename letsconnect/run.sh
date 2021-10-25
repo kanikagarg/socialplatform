9b1cfdbf642d44fdbd6325f9e04320baeceb7aff
Kanika Garg <reachkanikagarg@gmail.com>
2021-10-26 00:57:44 +0530

    updated shell file

--- letsconnect/run.sh
+++ letsconnect/run.sh
@@ -1,1 +1,6 @@
+python manage.py makemigrations
+python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('squareboat', 'support@squareboat.com', 'squareboat')" | python manage.py shell
+django-admin loaddata  db.json -i
 python manage.py runserver
+
